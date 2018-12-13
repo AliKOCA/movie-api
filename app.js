cdffe546c56edcaf85a30a84be9e4cf98ac51829
asg <asg@localhost.localdomain>
2018-12-13 16:40:25 +0300

    Ilerliyoruz.

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const movieRouter = require('./routes/movie');
 
 const app = express();
+//db connection
+const db = require("./helper/db.js")();//export edilen fonksiyonu direk çalıştırır.
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +22,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/movie/', movieRouter);
+
+app.get("/api", (req, res) => {
+  res.send("api");
+});
+
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -28,7 +35,7 @@ app.use((req, res, next) => {
 });
 
 // error handler
-app.use((err, req, res, next) =>  {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ helper/db.js
@@ -0,0 +1,14 @@
+//mongodb://<dbuser>:<dbpassword>@ds117164.mlab.com:17164/movie-api
+const mongoose = require("mongoose");
+
+module.exports = () => {
+    mongoose.connect("mongodb://localhost/movie-api",
+        { useNewUrlParser: true });
+    mongoose.connection.on('open', () => {
+        console.log("MongoDB: Bağlantı sağlandı.");
+    });
+    mongoose.connection.on('error', (err) => {
+        console.log("MongoDB Bağlantı Hatası: " + err);
+    });
+
+}
