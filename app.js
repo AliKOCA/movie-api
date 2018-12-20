342a3c38b4fd4b3f8c2f6dbba42b9240d87b8f09
asg <asg@localhost.localdomain>
2018-12-20 11:15:16 +0300

    mocha test basladi

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ const db = require("./helper/db.js")();//export edilen fonksiyonu direk çalış
 const ayarlar = require("./config")
 app.set('api_secret_key', ayarlar.api_secret_key);
 
+//Middleware
+const verifyToken = require("./middleware/verify-token")
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -27,6 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api', verifyToken);
 app.use('/api/movies/', movieRouter);
 app.use('/api/directors/', directorRouter);
 
